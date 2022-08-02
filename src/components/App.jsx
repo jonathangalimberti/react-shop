6185825aacbcc0708f62d8a3b0f833b7903121f6
jona-11 <jonathangalimberti@hotmail.com>
2022-08-02 20:24:46 -0300

    install webpack, babel and configure it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import '../styles/global.scss';
 
 const App = () =>{
     return(
-        <h1>Hola Mundo</h1>
+        <h1>Hola Mundo, terminando de configurar react y webpack!!</h1>
+       
     )
 }
 
